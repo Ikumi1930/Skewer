70b8de1d1dd643332e577598da38ec3f0ac1f5d8
k022g0085@g.neec.ac.jp <k022g0085@g.neec.ac.jp>
2023-09-13 21:27:51 +0900

    タイトルバーを変えた

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"2028_串刺し戦記");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
